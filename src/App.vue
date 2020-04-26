6531a92b85a5e0952f88671559cb2a27e9c30f9b
gman112 <64228119+gman112@users.noreply.github.com>
2020-04-26 01:23:29 +0100

    search function added
    users can now search for specific country

--- src/App.vue
+++ src/App.vue
@@ -35,17 +35,136 @@
         v-model="input"
         type="text"
         placeholder="search by country"
-        class="w-full rounded shadow p-5"
-        v-on:keyup.enter="searchCountry()"
+        class="w-full rounded shadow p-5 capitalize"
       />
     </div>
     <div class="flex justify-center mt-5 mb-5">
-      <Table
-        :data="country.data"
-        :showAllData="showAllData"
-        :showFilteredData="showFilteredData"
-        :filteredData="filteredData"
-      />
+      <table class="table-auto border-collapse flex-grow ml-16 mr-16 bg-white shadow">
+        <thead>
+          <tr>
+            <th class="px-4 py-2">Rank</th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Country
+                <div class="flex p-0 m-0">
+                  <i class="fas fa-arrow-up mt-1 ml-2" v-on:click="sortCountryByDesc(country.data)"></i>
+                  <i
+                    class="fas fa-arrow-down mt-1 ml-2"
+                    v-on:click="sortCountryByAsc(country.data)"
+                  ></i>
+                </div>
+              </div>
+            </th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Cases
+                <div class="flex p-0 m-0">
+                  <i class="fas fa-arrow-up mt-1 ml-2" v-on:click="sortCasesByDesc(country.data)"></i>
+                  <i class="fas fa-arrow-down mt-1 ml-2" v-on:click="sortCasesByAsc(country.data)"></i>
+                </div>
+              </div>
+            </th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Deaths
+                <div class="flex p-0 m-0">
+                  <i class="fas fa-arrow-up mt-1 ml-2" v-on:click="sortDeathsByDesc(country.data)"></i>
+                  <i class="fas fa-arrow-down mt-1 ml-2" v-on:click="sortDeathsByAsc(country.data)"></i>
+                </div>
+              </div>
+            </th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Critical
+                <div class="flex p-0 m-0">
+                  <i
+                    class="fas fa-arrow-up mt-1 ml-2"
+                    v-on:click="sortCriticalByDesc(country.data)"
+                  ></i>
+                  <i
+                    class="fas fa-arrow-down mt-1 ml-2"
+                    v-on:click="sortCriticalByAsc(country.data)"
+                  ></i>
+                </div>
+              </div>
+            </th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Recovered
+                <div class="flex p-0 m-0">
+                  <i
+                    class="fas fa-arrow-up mt-1 ml-2"
+                    v-on:click="sortRecoveredByDesc(country.data)"
+                  ></i>
+                  <i
+                    class="fas fa-arrow-down mt-1 ml-2"
+                    v-on:click="sortRecoveredByAsc(country.data)"
+                  ></i>
+                </div>
+              </div>
+            </th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Today's Cases
+                <div class="flex p-0 m-0">
+                  <i
+                    class="fas fa-arrow-up mt-4 ml-2"
+                    v-on:click="sortTodayCasesByDesc(country.data)"
+                  ></i>
+                  <i
+                    class="fas fa-arrow-down mt-4 ml-2"
+                    v-on:click="sortTodayCasesByAsc(country.data)"
+                  ></i>
+                </div>
+              </div>
+            </th>
+            <th class="px-4 py-2">
+              <div class="flex">
+                Today's Deaths
+                <div class="flex p-0 m-0">
+                  <i
+                    class="fas fa-arrow-up mt-4 ml-2"
+                    v-on:click="sortTodayDeathByDesc(country.data)"
+                  ></i>
+                  <i
+                    class="fas fa-arrow-down mt-4 ml-2"
+                    v-on:click="sortTodayDeathByAsc(country.data)"
+                  ></i>
+                </div>
+              </div>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(country, index) in filteredCountry" :key="country.countryInfo._id">
+            <td class="border px-4 py-2">{{index +1}}</td>
+            <td class="border px-4 py-2">
+              <div class="flex">
+                <img :src="country.countryInfo.flag" class="object-contain w-6 h-4 mr-4" alt />
+                {{country.country}}
+              </div>
+            </td>
+            <td
+              class="border px-4 py-2 tracking-wide font-hairline text-sm"
+            >{{country.cases | numeralFormat}}</td>
+            <td
+              class="border px-4 py-2 tracking-wide font-hairline text-sm"
+            >{{country.deaths| numeralFormat}}</td>
+            <td
+              class="border px-4 py-2 tracking-wide font-hairline text-sm"
+            >{{country.critical| numeralFormat}}</td>
+            <td
+              class="border px-4 py-2 tracking-wide font-hairline text-sm"
+            >{{country.recovered| numeralFormat}}</td>
+            <td
+              class="border px-4 py-2 tracking-wide font-hairline text-sm"
+            >{{country.todayCases| numeralFormat}}</td>
+            <td
+              class="border px-4 py-2 tracking-wide font-hairline text-sm"
+            >{{country.todayDeaths| numeralFormat}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -53,17 +172,12 @@
 <script>
 import "../node_modules/@fortawesome//fontawesome-free/css/all.css";
 import axios from "axios";
-
 import Card from "../src/components/Card";
-import Table from "./components/Table";
 
 export default {
   data() {
     return {
       input: "",
-      showAllData: true,
-      showFilteredData: false,
-      filteredData: {},
       country: {
         data: []
       },
@@ -86,21 +200,106 @@ export default {
     };
   },
   components: {
-    Card,
-    Table
+    Card
   },
   methods: {
-    searchCountry() {
-      axios
-        .get(
-          "https://corona.lmao.ninja/v2/countries/" +
-            this.input +
-            "?yesterday=false"
-        )
-        .then(res => {
-          this.filteredData = res;
-        });
-      (this.showAllData = false), (this.showFilteredData = true);
+    sortCasesByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.cases > b.cases) {
+          return -1;
+        }
+      });
+    },
+    sortCasesByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.cases > a.cases) {
+          return -1;
+        }
+      });
+    },
+    sortDeathsByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.deaths > b.deaths) {
+          return -1;
+        }
+      });
+    },
+    sortDeathsByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.deaths > a.deaths) {
+          return -1;
+        }
+      });
+    },
+    sortCriticalByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.critical > b.critical) {
+          return -1;
+        }
+      });
+    },
+    sortCriticalByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.critical > a.critical) {
+          return -1;
+        }
+      });
+    },
+    sortRecoveredByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.recovered > b.recovered) {
+          return -1;
+        }
+      });
+    },
+    sortRecoveredByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.recovered > a.recovered) {
+          return -1;
+        }
+      });
+    },
+    sortCountryByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.country > b.country) {
+          return -1;
+        }
+      });
+    },
+    sortCountryByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.country > a.country) {
+          return -1;
+        }
+      });
+    },
+    sortTodayCasesByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.todayCases > b.todayCases) {
+          return -1;
+        }
+      });
+    },
+    sortTodayCasesByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.todayCases > a.todayCases) {
+          return -1;
+        }
+      });
+    },
+    sortTodayDeathByDesc(arr) {
+      arr.sort((a, b) => {
+        if (a.todayDeaths > b.todayDeaths) {
+          return -1;
+        }
+      });
+    },
+    sortTodayDeathByAsc(arr) {
+      arr.sort((a, b) => {
+        if (b.todayDeaths > a.todayDeaths) {
+          return -1;
+        }
+      });
     }
   },
   mounted() {
@@ -135,6 +334,15 @@ export default {
         }
         this.showAllData = true;
       });
+  },
+  computed: {
+    filteredCountry() {
+      return this.country.data.filter(country => {
+        return country.country.match(
+          this.input.charAt(0).toUpperCase() + this.input.slice(1)
+        );
+      });
+    }
   }
 };
 </script>
